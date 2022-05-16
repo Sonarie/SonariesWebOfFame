67afd9a4ff651298cc532aec629cfc1e885463b4
Sonarie <sonarie1980@gmail.com>
2022-05-15 23:09:15 -0500

    added last 3 projects

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -14,16 +14,39 @@ const Portfolio = ({ category }) => {
       link: "https://Sonarie.github.io/run-buddy/"  
     },
     {
+      name: 'Password Generator',
+      category: 'Phase 1',
+      description:
+        'This app allows users to generate a random password that meets determined criteria set by the user',
+      link: "https://sonarie.github.io/RandomPassword/"
+    },
+    {
       name: 'DateLime',
       category: 'Phase 1',
       description:
-        'This was our Project for Phase 1. This website allowed users to chose a movie and a recipe to create date night'
+        'This was our Project for Phase 1. This website allowed users to chose a movie and a recipe to create date night',
+      link: "https://sonarie.github.io/Seven/"
+    },
+    {
+      name: 'Note Taker',
+      category: 'Phase 2',
+      description:
+        'This note taker app allows users to write and save notes to organize their thoughts',
+      link: "https://sonarie-notetaker.herokuapp.com/"
     },
     {
       name: 'Festival of Nations',
       category: 'Phase 2',
       description:
-        'This was our Project for Phase 2. This website allowed users to sign up for a cultural celebration by representing a country and chosing how they were going to showcase'
+        'This was our Project for Phase 2. This website allowed users to sign up for a cultural celebration by representing a country and chosing how they were going to showcase',
+      link: "https://powerful-mesa-61465.herokuapp.com/"  
+    },
+    {
+      name: 'Budget Tracker',
+      category: 'Phase 3',
+      description:
+        'This budget tracker allowed users to track there subtractions and deductions from their budget',
+      link: "https://lit-beyond-87910.herokuapp.com/"
     },
   ]);
 
--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -5,7 +5,6 @@ export default function Resume() {
     return(
         <section className='my-5'>
             <h1 id="resume">Resume</h1>
-            <div className="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="light" data-type="VERTICAL" data-vanity="sonjaliveoak" data-version="v1"><a className="badge-base__link LI-simple-link" href="https://www.linkedin.com/in/sonjaliveoak?trk=profile-badge">Sonja Watson</a></div>
             <div>
             <a href={require("../../assets/docs/resume.pdf")} target="_blank" rel="noreferrer" className="resume">Open Here</a>
             </div>        
