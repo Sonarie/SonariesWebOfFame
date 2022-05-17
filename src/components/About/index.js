f0a7f753061484fc5f9528aadb80fc9dda15d82e
Sonarie <sonarie1980@gmail.com>
2022-05-17 18:10:14 -0500

    minor updates

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -9,12 +9,12 @@ export default function About() {
         <p>
           I recently added to my web of skills by completing a full stack development 
           bootcamp through the University of Minnesota.  I'm always looking to continuously 
-          grow and develop in my field of Training and Development where I work out the bugs to 
+          grow and develop in my field of Training and Development where I work to 
           create engaging user friendly training content in many areas of studu. My creative platform 
           is the Adobe Creative Suite + Captivate. I will be able to merge coding with my 
           expertise in training, process improvement and design will be exciting adventure to 
-          creating new content for users. This portfolio is my showcase to project I have 
-          developed.
+          creating new content for users. This portfolio is my showcase to projects I have 
+          developed or collaborated with.
         </p>
     </div>
   </section>
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -3,34 +3,34 @@ import React from 'react';
 function Nav({ currentPage, handlePageChange }) {
   return (
     <ul className="flex-row px-1">
-      <h2><span role="img" aria-label="web">🕸️</span>Web of Fame</h2>
+      <h2><span role="img" aria-label="web"></span>Sonja Watson</h2>
       <ul className="flex-row">
         <li className="mx-2">
           <a href="#about" onClick={() => handlePageChange('About')}
             className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
           >
-            Who I Am
+            About Me
           </a>
         </li>
         <li className="mx-2">
           <a href="#portfolio" onClick={() => handlePageChange('Portfolio')}
             className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
           >
-            What I Do
+            Portfolio
           </a>
         </li>
         <li className="mx-2">
           <a href="#resume" onClick={() => handlePageChange('Resume')}
             className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
           >
-            What I've Done
+            Resume
           </a>
         </li>
         <li className="mx-2">
           <a href="#contactForm" onClick={() => handlePageChange('Contact')}
             className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
           >
-            Contact Me
+            Contact
           </a>
         </li>
       </ul>
